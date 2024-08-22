1589047a405d34a6e9ee35dc15ec5bc418bdf68e
Joao Cruz <joaovcruz50@gmail.com>
2024-08-22 13:15:46 -0300

    Commit01

--- src/assets/Header.tsx
+++ src/assets/Header.tsx
@@ -3,21 +3,27 @@ import Navbar from '../components/Navbar';
 import '../styles/header.css';
 
 type HeaderProps = {
-  title: string;
-  subtitle: string;
   logoUrl: string;
+  actionButtons: { label: string; url: string }[];
+
 };
 
-const Header: React.FC<HeaderProps> = ({ title, subtitle, logoUrl }) => {
+const Header: React.FC<HeaderProps> = ({
+  logoUrl,
+  actionButtons,
+}) => {
   return (
     <header>
       <div className="top-section">
         <div className="logo">
           {logoUrl && <img src={logoUrl} alt="Logo" />}
         </div>
-        <div className="text">
-          <h1>{title}</h1>
-          <h2>{subtitle}</h2>
+        <div className="actions">
+          {actionButtons.map((button, index) => (
+            <button key={index} className="action-button">
+              {button.label}
+            </button>
+          ))}
         </div>
       </div>
       <Navbar />
