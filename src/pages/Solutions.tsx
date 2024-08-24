3e49b51ec7dff5f00e22b4d19ae850f9f3f79007
Your Name <joaovcruz50@gmail.com>
2024-08-24 18:14:14 -0300

    commit04

---
+++ src/pages/Solutions.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Solutions: React.FC = () => {
+  return (
+    <div>
+      <h1>Solutions Page</h1>
+      <p>Welcome to the Solutions page.</p>
+    </div>
+  );
+};
+
+export default Solutions;  // Exportação padrão
+
